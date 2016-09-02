7440fe19ef0c5745621d20492af341e739ba6c4d
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2016-09-02 15:40:31 +0200

    NXP-19326 - Moved components restart in RuntimeDeployment + added hot deployment support in RuntimeFeature

--- nuxeo-core/nuxeo-core-test/src/main/java/org/nuxeo/ecm/core/test/CoreFeature.java
+++ nuxeo-core/nuxeo-core-test/src/main/java/org/nuxeo/ecm/core/test/CoreFeature.java
@@ -52,8 +52,10 @@ import org.nuxeo.ecm.core.work.api.WorkManager;
 import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.jtajca.NuxeoContainer;
 import org.nuxeo.runtime.model.URLStreamRef;
+import org.nuxeo.runtime.reload.ReloadService;
 import org.nuxeo.runtime.test.runner.Defaults;
 import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.HotDeployer;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.LocalDeploy;
@@ -72,6 +74,7 @@ import com.google.inject.Provider;
  */
 @Deploy({ "org.nuxeo.runtime.management", //
         "org.nuxeo.runtime.metrics",
+        "org.nuxeo.runtime.reload", // required by #CoreDeployer
         "org.nuxeo.ecm.core.schema", //
         "org.nuxeo.ecm.core.query", //
         "org.nuxeo.ecm.core.api", //
@@ -149,6 +152,8 @@ public class CoreFeature extends SimpleFeature {
 
     @Override
     public void initialize(FeaturesRunner runner) {
+    	runner.getFeature(RuntimeFeature.class).onHotDeploy(new CoreDeployer());
+
         storageConfiguration = new StorageConfiguration(this);
         txFeature = runner.getFeature(TransactionalFeature.class);
         txFeature.addWaiter(new WorksWaiter());
@@ -398,4 +403,15 @@ public class CoreFeature extends SimpleFeature {
         return session;
     }
 
+    class CoreDeployer extends HotDeployer.DeployAction {
+		@Override
+		public void deploy(String... contribs) throws Exception {
+			waitForAsyncCompletion();
+	        releaseCoreSession();
+			next.deploy(contribs);
+			Framework.getService(ReloadService.class).reloadRepository();
+	        createCoreSession();
+
+		}
+    }
 }
